905488254ed2c2146d76c31845e1b1f99f610625
Marwa <marwa.maghnie@gmail.com>
2025-10-20 23:00:39 +0300

    chore(ui): change to a more modern font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import '@fontsource/cairo/400.css'
+import '@fontsource/cairo/600.css'
+import '@fontsource/cairo/700.css'
 import './index.css'
 import App from './App.tsx'
 
